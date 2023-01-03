9810d1a1ba473bad4939e4308681dc3248086a05
SzentgyorgyiBernad <szentgyorgyi.bernad@student.ms.sapientia.ro>
2023-01-03 22:38:09 +0200

    Labor12

--- lab11/main_11.cpp
+++ lab11/main_11.cpp
@@ -1,6 +1,103 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void labor11();
+string toLower(string word);
 
 int main() {
-    std::cout << "Hello, Lab_11" << std::endl;
+    labor11();
+
     return 0;
 }
+
+string toLower(string word)
+{
+    string newS;
+    for(char i:word)
+    {
+        newS+= tolower(i);
+    }
+    return newS;
+}
+void labor11()
+{
+
+
+    ifstream f("bemenet.txt");
+
+    if(!f)
+    {
+        cout<<"File ERROR!\n";
+        return;
+    }
+
+    map<string,vector<int>> words{};
+    string rows;
+
+    bool world_key=true;
+    int index=1;
+
+    while(getline(f,rows))
+    {
+        if(rows == "\0")
+        {
+            world_key=false;
+            continue;
+        }
+
+        if(world_key)
+        {
+            words.insert({toLower(rows),{}});
+        }
+        else
+        {
+            string help{};
+            for(char c:toLower(rows) + '\n')
+            {
+                if(c == ' ' || c == '\n' )
+                {
+                    if(words.find(help) != words.end())
+                    {
+                        if(find(words[help].begin(), words[help].end(), index) == words[help].end())
+                        {
+                            words[help].push_back(index);
+                        }
+                    }
+                    help="";
+                    continue;
+                }
+                if(c>='a' && c<='z')
+                {
+                    help+=c;
+                }
+            }
+            index++;
+        }
+        rows=" ";
+    }
+    int b=-2;
+    for(auto x:words)
+    {
+        cout<<x.first<<" ";
+        for(auto w:x.second)
+        {
+            if(w==b+1)
+            { cout<<"- "<<w<<" ";}
+             else cout<<w<<" ";
+            b=w;
+        }
+        cout<<endl;
+    }
+
+    f.close();
+}
+
+
+
+
